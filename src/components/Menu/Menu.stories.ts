858f0aa0c23d26478653d4c25afae24574534d40
chenzj <chjczj@126.com>
2021-07-18 02:55:20 +0800

    ✨ add table

--- src/components/Menu/Menu.stories.ts
+++ src/components/Menu/Menu.stories.ts
@@ -19,79 +19,79 @@ export const Menu = MenuObj.bind({})
 
 Menu.args = {
   list: [{
-    name: '一级导航1',
+    name: '一级菜单1',
     index: '1',
     children: [{
-      name: '二级导航1',
+      name: '二级菜单1',
       index: '1-1',
       children: [{
-        name: '三级导航1',
+        name: '三级菜单1',
         index: '1-1-1',
         icon: 's-opportunity'
       },{
-        name: '三级导航2',
+        name: '三级菜单2',
         index: '1-1-2',
       },{
-        name: '三级导航3',
+        name: '三级菜单3',
         index: '1-1-3',
       },{
-        name: '三级导航4',
+        name: '三级菜单4',
         index: '1-1-4',
       },{
-        name: '三级导航5',
+        name: '三级菜单5',
         index: '1-1-5',
       },{
-        name: '三级导航6',
+        name: '三级菜单6',
         index: '1-1-6',
       },{
-        name: '三级导航7',
+        name: '三级菜单7',
         index: '1-1-7',
       },{
-        name: '三级导航8',
+        name: '三级菜单8',
         index: '1-1-8',
       },]
     },{
-      name: '二级导航2',
+      name: '二级菜单2',
       index: '1-2'
     },{
-      name: '二级导航3',
+      name: '二级菜单3',
       index: '1-3'
     },{
-      name: '二级导航4',
+      name: '二级菜单4',
       index: '1-4'
     },{
-      name: '二级导航5',
+      name: '二级菜单5',
       index: '1-5'
     },{
-      name: '二级导航6',
+      name: '二级菜单6',
       index: '1-6'
     },{
-      name: '二级导航7',
+      name: '二级菜单7',
       index: '1-7'
     },{
-      name: '二级导航8',
+      name: '二级菜单8',
       index: '1-8'
     },]
   },{
-    name: '一级导航2',
+    name: '一级菜单2',
     index: '2',
   },{
-    name: '一级导航3',
+    name: '一级菜单3',
     index: '3',
   },{
-    name: '一级导航4',
+    name: '一级菜单4',
     index: '4',
   },{
-    name: '一级导航5',
+    name: '一级菜单5',
     index: '5',
   },{
-    name: '一级导航6',
+    name: '一级菜单6',
     index: '6',
   },{
-    name: '一级导航7',
+    name: '一级菜单7',
     index: '7',
   },{
-    name: '一级导航8',
+    name: '一级菜单8',
     index: '8',
   },],
   router: false
---
+++ src/components/Table/Table.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="space-y-2">
+    <el-table border :data="data">
+      <template v-for="(column, index) in columns">
+        <el-table-column
+          v-bind="column"
+          :key="keyName ? column[keyName] : index"
+        />
+      </template>
+    </el-table>
+    <el-pagination
+      background
+      layout="prev, pager, next"
+      :total="total ? total : data.length"
+      class="text-right"
+    >
+    </el-pagination>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    columns: {
+      type: Array,
+      required: true,
+    },
+    data: {
+      type: Array,
+      required: true,
+    },
+    keyName: {
+      type: String,
+      required: false,
+      default() {
+        return ''
+      },
+    },
+  },
+  setup() {},
+})
+</script>
