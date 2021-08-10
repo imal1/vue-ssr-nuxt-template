d93d77d843db57078d7278ef0d5c668f9167e71e
imali <chjczj@126.com>
2021-08-10 16:41:36 +0800

    🐛 fix http hook

--- src/plugins/http.ts
+++ src/plugins/http.ts
@@ -12,27 +12,25 @@ export default async function ({ $http, $message }: any) {
   await getHost($http)
 
   $http.onResponse(async (_req: any, _options: any, res: any) => {
-    if (process.client) {
-      switch (res.status) {
-        case (500): {
-          throw new Error('服务器错误')
-        }
-        default: {
-          const { code, msg } = await res.json()
-          if (code.toString() !== '200') {
-            $message.error(msg)
-          }
+    switch (res.status) {
+      case (500): {
+        throw new Error('服务器错误')
+      }
+      case (404): {
+        throw new Error('找不到服务器')
+      }
+      default: {
+        const { code, msg } = await res.json()
+        if (code.toString() !== '200') {
+          process.client && $message.error(msg)
         }
       }
     }
   })
 
   $http.onError((error: any) => {
-    if (process.client && error.statusCode === 500) {
-      $message.error('服务器错误')
-    } else {
-      $message.error(error.message)
-    }
+    process.client && $message.error(error.message)
+    return {}
     // Tip: error.response will be undefined if the connection dropped to the server
     // Tip: You can use error.response.data to get response message
     // Tip: You can return an object or Promise as fallback response to avoid rejection
